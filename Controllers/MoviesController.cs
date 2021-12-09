96fad26b85c2644a058ed339cdcd3dd272bbc15f
rBannatyne <ruaridhi.bannatyne@gmail.com>
2021-12-09 10:07:39 +0000

    Adding swagger and annotations for API methods.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -12,6 +12,7 @@ namespace MovieAPI.Controllers
 {
     [Route("api/Movies")]
     [ApiController]
+   // [Produces("application/json")]
     public class MoviesController : ControllerBase
     {
         private readonly MovieContext _context;
@@ -67,9 +68,15 @@ namespace MovieAPI.Controllers
 
         }
 
+        /// <summary>
+        /// Gets all movie records
+        /// </summary>
+        /// <returns>Array of MovieDTO objects</returns>
+        /// <response code="200">All records returned as list of MovieDTOs in json</response>
         // GET: api/Movies
         [HttpGet]
         [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<MovieDTO>>> GetMovies()
         {
             return await _context.Movies
@@ -77,9 +84,20 @@ namespace MovieAPI.Controllers
                 .ToListAsync();
         }
 
+        /// <summary>
+        /// Returns existing movie record with ID passed in, if any
+        /// </summary>
+        /// <param name="id">ID of existing record to be updated.</param>
+        /// <returns>Nothing</returns>
+        /// <response code="200">A MovieDTO with ID equal to id parameter returned</response>
+        /// <response code="400">ID parameter passed in invalid</response>
+        /// <response code="404">No movie with ID equal to id parameter found </response>
         // GET: api/Movies/5
         [HttpGet("{id}")]
         [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<MovieDTO>> GetMovie(long id)
         {
             var movie = await _context.Movies.FindAsync(id);
@@ -92,8 +110,21 @@ namespace MovieAPI.Controllers
             return MovieToDTO(movie);
         }
 
+        /// <summary>
+        /// Overwrites the existing movie with ID in URL with data passed in as MovieDTO
+        /// </summary>
+        /// <param name="id">ID of existing record to be updated.</param>
+        /// <param name="movieDTO">MovieDTO containing data to be used to update existing record.</param>
+        /// <returns>Nothing</returns>
+        /// <response code="204">A movie with ID equal to id parameter found and updated, nothing returned</response>
+        /// <response code="400">Input invalid: ID in URL did not match that in MovieDTO passed in, or data in MovieDTO not valid</response>
+        /// <response code="404">No movie with ID equal to id parameter found </response>
+        /// 
         // PUT: api/Movies/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpPut("{id}")]
         public async Task<IActionResult> PutMovie(long id, MovieDTO movieDTO)
         {
@@ -132,8 +163,17 @@ namespace MovieAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Creates a new movie record using MovieDTO parameter passed in
+        /// </summary>
+        /// <param name="movieDTO">MovieDTO containing data to be used to create new record.</param>
+        /// <returns>MovieDTO containing record created </returns>
+        /// <response code="204">A movie with ID equal to id parameter found and updated, nothing returned</response>
+        /// <response code="400">Input invalid: data in MovieDTO not valid</response>
         // POST: api/Movies
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
         public async Task<ActionResult<Movie>> PostMovie(MovieDTO movieDTO)
         {
@@ -160,8 +200,19 @@ namespace MovieAPI.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Deletes the movie specified in the id parameter, if found
+        /// </summary>
+        /// <param name="id">ID of the movie to delete</param>
+        /// <returns>Nothing</returns>
+        /// <response code="204">A movie with ID equal to id parameter found and deleted, no content returned</response>
+        /// <response code="404">No movie with ID equal to id parameter found </response>
+        /// 
         // DELETE: api/Movies/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteMovie(long id)
         {
             var movie = await _context.Movies.FindAsync(id);
--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MovieAPI.Models;
+using System.Reflection;
+using System.IO;
+using System;
 
 namespace MovieAPI
 {
@@ -37,11 +41,26 @@ namespace MovieAPI
 
                     });
                 });
-
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MovieAPI", Version = "v1" });
-            //});
+            services.AddSwaggerGen( options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "MovieAPI",
+                    Description = "Simple .NET Core API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "MovieAPI",
+                        Url = new System.Uri("https://github.com/rbanna01/MovieAPI")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Dummy license"
+                    }
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,8 +69,8 @@ namespace MovieAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieAPI v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
